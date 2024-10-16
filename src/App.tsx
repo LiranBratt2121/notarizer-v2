e4cb7eb40a28e9bc8d97ae6d28ee1cd7995868f4
LiranBratt2121 <brattlirannin@gmail.com>
2024-10-16 23:45:50 +0300

    Add homepage inside app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
-import Navbar from './components/navbar/Navbar';
-import Hero from './components/hero/Hero';
 import GlobalStyle from './styles/globalStyles';
+import HomePage from './pages/homepage/HomePage';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Navbar />
-      <Hero />
+      <HomePage />
       {/* Other components */}
     </ThemeProvider>
   );
