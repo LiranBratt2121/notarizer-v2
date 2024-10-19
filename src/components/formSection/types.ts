5d6cd0e109eac09976338a709ef32ee0af337e37
LiranBratt2121 <brattlirannin@gmail.com>
2024-10-19 16:17:43 +0300

    Encapsulated FormContent

--- src/components/formSection/types.ts
+++ src/components/formSection/types.ts
@@ -1,10 +1,11 @@
 interface FormSectionProps {
     title: string;
     subTitle: string;
-    forms: Array<Array<{lable: string, type: FormTypeOptions; placeholder: string}>>;
+    forms: FormContent
     bgColor?: string;
 }
 
+type FormContent = Array<Array<{ lable: string, type: FormTypeOptions; placeholder: string }>>;
 type FormTypeOptions = 'text' | 'password' | 'email' | 'number';
 
-export type { FormSectionProps };
+export type { FormSectionProps, FormContent };
