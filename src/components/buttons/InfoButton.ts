0510360ebfe78ca056e1aebb3e98d4e536135d7f
LiranBratt2121 <brattlirannin@gmail.com>
2024-10-24 22:12:52 +0300

    Make InfoButton dynamic

--- src/components/buttons/InfoButton.ts
+++ src/components/buttons/InfoButton.ts
@@ -3,13 +3,17 @@ import styled from "styled-components";
 export const InfoButton = styled.button`
     background-color: ${props => props.theme.colors.primaryLight};
     color: ${(props) => props.theme.colors.primary};
-    padding: 0.8rem, 1.5rem;
+    padding: 0.5rem;
+    width: 10vw;
+    height: 30px;
     border: none;
     border-radius: 4px;
     cursor: pointer;
     font-size: ${(props) => props.theme.fontSizes.medium};
+    min-width: 100px;
+    min-height: 30px;
 
     &:hover {
         background-color: ${(props) => props.theme.colors.seconderyLight};
     }
-`
+`;
