6075926ac6f34be9d20b1b80579cf67b0218efc0
LiranBratt2121 <brattlirannin@gmail.com>
2024-10-24 13:28:55 +0300

    Add routes for getting started

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import GlobalStyle from './styles/globalStyles';
+import GettingStartedLandlord from './pages/gettingStarted/GettingStartedLandlord';
 import HomePage from './pages/homepage/HomePage';
+import GettingStarted from './pages/gettingStarted/Choice'
+import GettingStartedTenant from './pages/gettingStarted/GettingStartedTenant';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/mvp-notarizer/" element={<HomePage />} />
+          <Route path="/mvp-notarizer/getting-started-landlord" element={<GettingStartedLandlord />} />
+          <Route path="/mvp-notarizer/getting-started-tenant" element={<GettingStartedTenant />} />
+          <Route path="/mvp-notarizer/getting-started" element={<GettingStarted />} />
+        </Routes>
+      </BrowserRouter>
       <GlobalStyle />
-      <HomePage />
-      {/* Other components */}
     </ThemeProvider>
   );
 };
