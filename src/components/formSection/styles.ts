657d87b319924133ac784a908add40190ea2e047
LiranBratt2121 <brattlirannin@gmail.com>
2024-10-24 13:05:06 +0300

    Add styles to fit radio feature

--- src/components/formSection/styles.ts
+++ src/components/formSection/styles.ts
@@ -8,43 +8,45 @@ const SectionContainer = styled.section<{ bgColor: string }>`
     background-color: ${props => props.bgColor};
     box-sizing: border-box;
     min-height: 100vh;    
-`
+`;
 
 const ContentWrapper = styled.div`
     flex: 1;
     max-width: 31.25rem;
     padding: 2rem;
-`
+`;
 
 const Title = styled.h2`
     font-size: ${props => props.theme.fontSizes.large};
     margin-bottom: 1.5rem;
     color: ${props => props.theme.colors.text};
-`
+`;
 
 const SubTitle = styled.h2`
     font-size: ${props => props.theme.fontSizes.medium};
     margin-bottom: 2rem;
     color: ${props => props.theme.colors.text};
-`
+`;
 
 const FormWrapper = styled.div`
     display: flex;
     flex-direction: column;
     gap: 1rem;
     margin-bottom: 2rem;
-`
+`;
 
 const FormField = styled.div`
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 `;
 
 const Lable = styled.label`
-    font-style: ${props => props.theme.fontSizes.medium};
+    font-size: ${props => props.theme.fontSizes.medium};
     color: ${props => props.theme.colors.text};
     margin-bottom: 0.5rem;
-`
+    display: flex; 
+    align-items: center; 
+`;
 
 const Input = styled.input`
     padding: 0.8rem;
@@ -53,12 +55,12 @@ const Input = styled.input`
     background-color: ${props => props.theme.colors.primaryLight};
     color: ${props => props.theme.colors.textDark};
     font-size: ${props => props.theme.fontSizes.medium};
-`
+    margin-right: 0.5rem; 
+`;
 
 const ButtonWrapper = styled.div`
     display: flex;
     justify-content: space-between;
-`
-
+`;
 
 export { SectionContainer, ContentWrapper, Title, SubTitle, FormWrapper, FormField, Lable, Input, ButtonWrapper };
