92ae226e3125f617ea1f561f9e8d3bda4c41f018
LiranBratt2121 <brattlirannin@gmail.com>
2024-10-24 13:04:32 +0300

    Add radio

--- src/components/formSection/types.ts
+++ src/components/formSection/types.ts
@@ -3,10 +3,12 @@ interface FormSectionProps {
     subTitle: string;
     forms: FormContent
     bgColor?: string;
+    handleSubmit: Function
 }
 
 type FormContent = Array<Array<{
     required: boolean, name: string; lable: string, type: FormTypeOptions; placeholder?: string, options?: { lable: string, value: string }[]
 }>>;
+type FormTypeOptions = 'text' | 'password' | 'email' | 'number' | 'radio';
 
 export type { FormSectionProps, FormContent };
