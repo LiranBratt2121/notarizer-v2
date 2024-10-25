3d4585996bab1f53cd53ce36c65de5f126463f37
LiranBratt2121 <brattlirannin@gmail.com>
2024-10-25 11:25:31 +0300

    Add gettingStartedTenant Template

---
+++ src/pages/gettingStarted/GettingStartedTenant.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormSection from '../../components/formSection/FormSection'
+
+const GettingStartedTenant = () => {
+    return (
+        <div>
+            Hello tenant
+        </div>
+    )
+}
+
+export default GettingStartedTenant
