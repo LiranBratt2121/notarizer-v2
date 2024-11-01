6e9352d8b4954dd7a4137b7c7189190be4bcd700
LiranBratt2121 <brattlirannin@gmail.com>
2024-11-01 18:05:28 +0200

    Load enviroment variables

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+require('dotenv').config();
+
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
   <App />
