3450c2f96f714245503bcfa066a3635faebd58ef
LiranBratt2121 <brattlirannin@gmail.com>
2024-11-02 17:41:41 +0200

    Init firebase

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { initFirebase } from './firebase/firebaseConfig.ts';
 
-require('dotenv').config();
-
+initFirebase();
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
   <App />
