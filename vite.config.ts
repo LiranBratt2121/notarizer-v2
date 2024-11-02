d97f55d55f13674b5995750b20f24af720b58bff
LiranBratt2121 <brattlirannin@gmail.com>
2024-11-02 17:33:35 +0200

    Remove https property as its not needed

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
-  server: { https: true }, // Not needed for Vite 5+
   plugins: [mkcert()]
 })
