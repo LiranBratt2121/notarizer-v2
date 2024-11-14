b3064b1232efd231c976d3e17f7d568f11febbba
LiranBratt2121 <brattlirannin@gmail.com>
2024-11-14 17:58:06 +0200

    Add register user function

--- src/firebase/registerUser.ts
+++ src/firebase/registerUser.ts
@@ -1,20 +1,36 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { getFirestore, setDoc, doc } from "firebase/firestore";
 import { User } from "./types"
+import { FirebaseStorage, getDownloadURL, getStorage, ref, uploadString } from "firebase/storage";
 
-export type RegisterUserData = Omit<User, 'id'> & { password: string; role: "landlord" | "tenant" };
+export type RegisterUserData = Omit<User, 'id'> & { password: string; };
 
-const registerUser = async (userData: RegisterUserData): Promise<void> => {
+const uploadB64Id = async (storage: FirebaseStorage, path: string, data: string) => {
+    const imageRef = ref(storage, path);
+    console.log(path.substring(0, 24));
+    await uploadString(imageRef, data, "data_url");
+
+    return await getDownloadURL(imageRef);
+}
+
+const registerUser = async (userData: RegisterUserData, role: "landlord" | "tenant"): Promise<void> => {
     const auth = getAuth();
     const db = getFirestore();
-
+    const storage = getStorage();
+    
     try {
         const userCredential = await createUserWithEmailAndPassword(auth, userData.email, userData.password);
         const userId = userCredential.user.uid;
+        
+        const idImageStoragePath = `ids/${userId}/images/${Date.now()}`;
+    
+        const downloadUrl = await uploadB64Id(storage, idImageStoragePath, userData.idImageString);
+        userData['idImageString'] = downloadUrl;
 
         await setDoc(doc(db, "users", userId), {
             ...userData,
             id: userId,
+            role
         });
 
         await signInWithEmailAndPassword(auth, userData.email, userData.password);
