e82e5b05058c21f4f202f2c574d94dc4e760237c
LiranBratt2121 <brattlirannin@gmail.com>
2024-11-14 17:59:38 +0200

    Add dashboard to navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import { NavLink } from './types';
 import { Logo, NavbarContainer, NavItem, NavLinks } from './styles';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
+    const navigate = useNavigate();
+
     const links: NavLink[] = [
         { id: 1, name: 'Home' },
         { id: 2, name: 'Services' },
-        { id: 3, name: 'Contact' },
-        { id: 4, name: 'About' }
+        { id: 3, name: 'About' },
+        { id: 4, name: 'Dashboard', navigate: "/mvp-notarizer/dashboard" },
     ];
 
     return (
         <NavbarContainer>
-        <Logo>Notarizer</Logo>
-        <NavLinks>
-        {
-        links.map((li) => (
-            <NavItem key= { li.id } > { li.name } </NavItem>
-        ))
-    }
-    </NavLinks>
-    </NavbarContainer>
-  );
+            <Logo>Notarizer</Logo>
+            <NavLinks>
+                {
+                    links.map((li) => (
+                        <NavItem key={li.id} onClick={() => navigate(li.navigate ?? "")}> {li.name} </NavItem>
+                    ))
+                }
+            </NavLinks>
+        </NavbarContainer>
+    );
 };
 
 export default Navbar;
