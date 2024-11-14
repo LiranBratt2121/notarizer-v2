0f71670cbfaf80cb912e71b66bd8c7ec42753cc0
LiranBratt2121 <brattlirannin@gmail.com>
2024-11-14 18:01:10 +0200

    Handle submit for landloard

--- src/pages/gettingStarted/GettingStartedLandlord.tsx
+++ src/pages/gettingStarted/GettingStartedLandlord.tsx
@@ -1,66 +1,78 @@
-import React from "react"
-import FormSection from "../../components/formSection/FormSection"
-import { FormContent } from "../../components/formSection/types"
-import { countries } from 'country-data-list'
+import React from "react";
+import FormSection from "../../components/formSection/FormSection";
+import { FormContent } from "../../components/formSection/types";
+import { countries } from 'country-data-list';
+import registerUser, { RegisterUserData } from "../../firebase/registerUser";
+import { useNavigate } from "react-router-dom";
 
 const GettingStartedLandlord = () => {
-    const handleSubmit = (data: Record<string, any>) => {
-        console.log("Submitted Data, ", data);
-    }
-    
+    const navigate = useNavigate();
+
+    const handleSubmit = async (data: Record<string, any>) => {
+        try {
+            await registerUser(data as RegisterUserData, "landlord");
+            alert(data.firstName + " Successfully registered!");
+        } catch (error) {
+            console.error("Registration error: ", error);
+            alert("Error during registration: " + error);
+        }
+
+        navigate('/mvp-notarizer/dashboard');
+    };
+
     const forms: FormContent = [
         [
             {
-                lable: "First Name", placeholder: "Enter your first name", type: "text", name: "First Name", required: true
+                lable: "First Name", placeholder: "Enter your first name", type: "text", name: "firstName", required: true
             },
             {
-                lable: "Last Name", type: "text", placeholder: "Enter your last name", name: "Last Name", required: true
+                lable: "Last Name", type: "text", placeholder: "Enter your last name", name: "lastName", required: true
             },
         ],
         [
             {
-                lable: "birthday", type: "date", name: "birthday", required: true
+                lable: "Birthday", type: "date", name: "birthday", required: true
             }
         ],
         [
             {
-                lable: "Email", type: "email", placeholder: "Enter your email", name: "Email", required: true
+                lable: "Email", type: "email", placeholder: "Enter your email", name: "email", required: true
             },
             {
-                lable: "Phone Number", type: "number", name: "PhoneNumber", required: true, placeholder: "Enter your phone number"
+                lable: "Phone Number", type: "number", name: "phoneNumber", required: true, placeholder: "Enter your phone number"
             }
         ],
         [
             {
-                lable: "Password", type: "password", placeholder: "Enter your password", name: "Password", required: true
+                lable: "Password", type: "password", placeholder: "Enter your password", name: "password", required: true
             },
             {
-                lable: "verify-Password", type: "password", placeholder: "Verify your password", name: "VerifyPassword", required: true
+                lable: "Verify Password", type: "password", placeholder: "Verify your password", name: "verifyPassword", required: true
             },
         ],
         [
             {
-                lable: "Enter ID", name: "ID", required: true, type: "file", accept: "image/*"
+                lable: "Enter ID", name: "idImageString", required: true, type: "file", accept: "image/*"
             }
         ],
         [
             {
-                lable: "Street-address", name: "StreetAddress", required: true, type: "text", placeholder: "Enter your street address" 
+                lable: "Street Address", name: "streetAddress", required: true, type: "text", placeholder: "Enter your street address"
             },
             {
-                lable: "ZIP-or-Postal-code", name: "ZipOrPostalCode", required: false, type: "number", placeholder: "Enter your ZIP code or pastal address (optional)" 
+                lable: "ZIP or Postal Code", name: "zipOrPostalCode", required: false, type: "number", placeholder: "Enter your ZIP code or postal address (optional)"
             },
             {
                 lable: "City", name: "city", required: true, type: "text", placeholder: "Enter your city"
             },
             {
-                lable: "Country or region", name: "CountryOrRegion", required: true, type: "dropDown",
+                lable: "Country or Region", name: "countryOrRegion", required: true, type: "dropDown",
                 options: countries.all.map((country) => (
                     { lable: country.name, value: country.name }
                 ))
             }
         ],
-    ]
+    ];
 
     return (
         <FormSection
@@ -69,7 +81,7 @@ const GettingStartedLandlord = () => {
             subTitle="Enter your information"
             handleSubmit={handleSubmit}
         />
-    )
-}
+    );
+};
 
-export default GettingStartedLandlord
+export default GettingStartedLandlord;
