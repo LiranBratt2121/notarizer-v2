8f4730e4641cc8de0fd17daa2da0796a03dfd64a
LiranBratt2121 <brattlirannin@gmail.com>
2024-11-14 21:13:04 +0200

    Add mobile support

--- src/components/buttons/InfoButton.ts
+++ src/components/buttons/InfoButton.ts
@@ -4,9 +4,6 @@ export const InfoButton = styled.button`
     background-color: ${props => props.theme.colors.primaryLight};
     color: ${(props) => props.theme.colors.primary};
     padding: 0.5rem 1rem;
-    width: 10vw;
-    height: 30px;
-    border: none;
     border-radius: 4px;
     cursor: pointer;
     font-size: ${(props) => props.theme.fontSizes.medium};
@@ -14,7 +11,7 @@ export const InfoButton = styled.button`
     min-height: 30px;
     text-align: center;
     transition: background-color 0.3s ease-in-out, transform 0.2s ease-in-out;
-    
+
     &:hover {
         background-color: ${(props) => props.theme.colors.seconderyLight};
     }
@@ -29,9 +26,8 @@ export const InfoButton = styled.button`
     }
 
     @media (max-width: 768px) {
-        font-size: ${(props) => props.theme.fontSizes.smaller};  // Adjust font size for mobile
-        padding: 0.5rem 0.8rem;  // Adjust padding for mobile screens
-        width: 25vw;  // Make the button more flexible for mobile screens
-        min-width: 80px;  // Adjust min-width for smaller screens
+        font-size: ${(props) => props.theme.fontSizes.small};
+        padding: 0.5rem 0.8rem;
+        width: auto;
     }
 `;
