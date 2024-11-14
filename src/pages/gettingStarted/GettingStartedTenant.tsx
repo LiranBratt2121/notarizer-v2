38668e52fb624de7ef35dd4b1bae7c5686c5c9c0
LiranBratt2121 <brattlirannin@gmail.com>
2024-11-14 18:00:49 +0200

    Add getting started page for tenant

--- src/pages/gettingStarted/GettingStartedTenant.tsx
+++ src/pages/gettingStarted/GettingStartedTenant.tsx
@@ -1,12 +1,69 @@
-import React from 'react'
-import FormSection from '../../components/formSection/FormSection'
+import React from "react";
+import FormSection from "../../components/formSection/FormSection";
+import { FormContent } from "../../components/formSection/types";
+import registerUser, { RegisterUserData } from "../../firebase/registerUser";
+import { useNavigate } from "react-router-dom";
 
 const GettingStartedTenant = () => {
-    return (
-        <div>
-            Hello tenant
-        </div>
-    )
-}
+    const navigate = useNavigate();
 
-export default GettingStartedTenant
+    const handleSubmit = async (data: Record<string, any>) => {
+        try {
+            await registerUser(data as RegisterUserData, "tenant");
+            alert(data.firstName + " Successfully registered!");
+        } catch (error) {
+            console.error("Registration error: ", error);
+            alert("Error during registration: " + error);
+        }
+
+        navigate('/mvp-notarizer/dashboard');
+    };
+
+    const forms: FormContent = [
+        [
+            {
+                lable: "First Name", placeholder: "Enter your first name", type: "text", name: "firstName", required: true
+            },
+            {
+                lable: "Last Name", type: "text", placeholder: "Enter your last name", name: "lastName", required: true
+            },
+        ],
+        [
+            {
+                lable: "Birthday", type: "date", name: "birthday", required: true
+            }
+        ],
+        [
+            {
+                lable: "Email", type: "email", placeholder: "Enter your email", name: "email", required: true
+            },
+            {
+                lable: "Phone Number", type: "number", name: "phoneNumber", required: true, placeholder: "Enter your phone number"
+            }
+        ],
+        [
+            {
+                lable: "Password", type: "password", placeholder: "Enter your password", name: "password", required: true
+            },
+            {
+                lable: "Verify Password", type: "password", placeholder: "Verify your password", name: "verifyPassword", required: true
+            },
+        ],
+        [
+            {
+                lable: "Enter ID", name: "idImageString", required: true, type: "file", accept: "image/*"
+            }
+        ],
+    ];
+
+    return (
+        <FormSection
+            forms={forms}
+            title="Tenant Registration"
+            subTitle="Enter your information"
+            handleSubmit={handleSubmit}
+        />
+    );
+};
+
+export default GettingStartedTenant;
