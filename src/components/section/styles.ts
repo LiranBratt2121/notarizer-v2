e6f5b317d3aec75a99ebe0f51777c512039ebe15
LiranBratt2121 <brattlirannin@gmail.com>
2024-11-14 21:16:37 +0200

    Add mobile support

--- src/components/section/styles.ts
+++ src/components/section/styles.ts
@@ -1,13 +1,39 @@
 import styled from 'styled-components';
 
+import styled from "styled-components";
+
 const SectionContainer = styled.section<{ bgColor: string }>`
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 4rem 2rem;
-  background-color: ${props => props.bgColor};
+  background-color: ${(props) => props.bgColor};
   box-sizing: border-box;
   min-height: 100vh;
+  width: 100vw;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 2rem 1rem;
+    justify-content: center;
+  }
+`;
+
+const DesktopOnly = styled.div`
+  display: flex;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const MobileOnly = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
 const ContentWrapper = styled.div`
@@ -19,6 +45,11 @@ const Title = styled.h2`
   font-size: ${props => props.theme.fontSizes.large};
   margin-bottom: 1rem;
   color: ${props => props.theme.colors.text};
+
+  @media (max-width: 768px) {
+      font-size: ${props => props.theme.fontSizes.bigger};
+      margin-bottom: 0.5rem;
+    }
 `;
 
 const Subtitle = styled.p<{ hasSubtitle2: boolean }>`
@@ -39,6 +70,13 @@ const ImageWrapper = styled.div<{ imageCount: number }>`
     gap: 1rem;
     max-width: 50%;
     margin-right: ${({ imageCount }) => (imageCount === 2) ? '3rem' : '0rem'};
+
+    @media (max-width: 768px) {
+      max-width: 100%;
+      margin-right: 0;
+      flex-direction: column;
+      gap: 0;
+    }
 `;
 
 const Image = styled.img`
@@ -46,6 +84,10 @@ const Image = styled.img`
   height: auto;
   border-radius: 1rem;
   object-fit: cover;
+
+  @media (max-width: 768px){
+    width: 100%;
+  }
 `;
 
-export { SectionContainer, ContentWrapper, Title, Subtitle, ButtonWrapper, ImageWrapper, Image }
+export { SectionContainer, ContentWrapper, Title, Subtitle, ButtonWrapper, ImageWrapper, Image, DesktopOnly, MobileOnly }
