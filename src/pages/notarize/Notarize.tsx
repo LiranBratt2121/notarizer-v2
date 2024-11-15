7a1959684c61ad806fea07df8763ca3a68b16308
LiranBratt2121 <brattlirannin@gmail.com>
2024-11-15 05:08:00 +0200

    Add notarizer page

---
+++ src/pages/notarize/Notarize.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import WebcamComponent from '../../components/webcam/Webcam';
+import uploadPropertyImage from '../../firebase/uploadPropertyImage';
+import { AddImageButton, NotarizeContainer, Title } from './styles';
+import { Camera } from 'lucide-react';
+import { waitForAuth } from '../../firebase/waitForAuth';
+import { getUserRole } from '../../firebase/getUserRole';
+
+const Notarize = () => {
+    const { propertyId } = useParams();
+    const [showCamera, setShowCamera] = useState(false);
+    const navigate = useNavigate();
+
+    const handleContinue = async (imageBase64: string) => {
+        if (!propertyId || !imageBase64) return;
+
+        try {
+            const uploaderId = await waitForAuth();
+
+            if (!uploaderId) {
+                navigate("/mvp-notarizer/");
+                alert("Please sign in before notarizing");
+                return;
+            }
+
+            const uploaderRole = await getUserRole(uploaderId);
+
+            if (!uploaderRole) {
+                navigate("/mvp-notarizer/");
+                alert("Uploader role was not tenant or landlord. strange!");
+                return;
+            }
+
+            console.log(propertyId)
+            await uploadPropertyImage(propertyId, imageBase64, uploaderId, uploaderRole);
+            setShowCamera(false);
+        } catch (error) {
+            console.error("Error handling image:", error);
+        }
+    };
+
+    return (
+        <NotarizeContainer>
+            <Title>Upload Property Images</Title>
+            <AddImageButton onClick={() => setShowCamera(true)}>
+                <Camera size={24} />
+                Add Images
+            </AddImageButton>
+
+            {showCamera && (
+                <WebcamComponent
+                    handleContiueInner={handleContinue}
+                    toNavigate={() => navigate(-1)}
+                    onClose={() => setShowCamera(false)}
+                />
+            )}
+        </NotarizeContainer>
+    );
+};
+
+export default Notarize;
